7bd1cf4b9ab5c8e6f647c83acfd3afdb0ba8c1e8
najah <najahkhaderr@gmail.com>
2024-05-07 17:28:34 +0200

    Descriptive commit message here

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/BLOG-APP/', // Update with your app name or custom base path
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify the output directory for production build
+  },
+});
